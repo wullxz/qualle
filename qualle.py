c792a37efc8bd4a586ba5fc08a8271ee9990d91e
Meik Specht <meik.specht@fh-bingen.de>
2015-02-21 00:48:39 +0000

    relatively secure Timeout for pictures

--- qualle.py
+++ qualle.py
@@ -7,24 +7,35 @@ import threading
 import RPi.GPIO as GPIO
 from pygame.locals import *
 
-waitTime=10
+waitTime=5
 
 img={}
+led={}
 T1=38
 T2=36
 dim=(0, 0) # dimensions of screen; initialized in init()
 zero=(0, 0) # zero position
+LEDON=0
+LEDOFF=1
+active=0
 
 def init():
+  global dim
   dinfo = pygame.display.Info()
   dim = (dinfo.current_w, dinfo.current_h)
   pygame.display.set_mode(dim, FULLSCREEN)
-  img['start'] = pygame.image.load("start.jpg")
-  img[T1] = pygame.image.load('b1.jpg')
-  img[T2] = pygame.image.load('b2.jpg')
+  # load pictures
+  img['start'] = loadImg("start.jpg")
+  img[T1] = loadImg('b1.jpg')
+  img[T2] = loadImg('b2.jpg')
   display = pygame.display.get_surface()
   display.blit(img['start'], zero)
   pygame.display.update()
+
+def loadImg(path):
+  image = pygame.image.load(path)
+  image = pygame.transform.scale(image, dim)
+  return image
 
 def main():
   pygame.init()
@@ -47,8 +58,15 @@ def setUpListener():
   GPIO.setup(T1, GPIO.IN)
   GPIO.setup(T2, GPIO.IN)
 
-  GPIO.add_event_detect(T1, GPIO.BOTH, callback=btn_handler, bouncetime=100)
-  GPIO.add_event_detect(T2, GPIO.BOTH, callback=btn_handler, bouncetime=100)
+  # define LEDs
+  led[T1] = 40
+  led[T2] = 37
+
+  for k,l in led.iteritems():
+    GPIO.setup(l, GPIO.OUT)
+
+  GPIO.add_event_detect(T1, GPIO.FALLING, callback=btn_handler, bouncetime=300)
+  GPIO.add_event_detect(T2, GPIO.FALLING, callback=btn_handler, bouncetime=300)
 
 def cleanup():
   GPIO.cleanup()
@@ -59,37 +77,55 @@ def quit():
   return
 
 def btn_handler(channel):
-  timer = Timeout()
-  timer.cancel()
-  display = pygame.display.get_surface()
-  print("Button press detected: " + str(channel))
-  # button press
-  if not GPIO.input(channel):
-    display.blit(img[channel], zero)
-    pygame.display.update()
-    timer.start()
+  global active
+  if active == channel:
+    # don't do anything when pic is already displayed
+    return
+  else:
+    active = channel
+    timer = Timeout()
+    timer.cancel()
+    display = pygame.display.get_surface()
+    # button press
+    if not GPIO.input(channel):
+      print("Button press detected: " + str(channel))
+      for k,l in led.iteritems():
+        if channel != k:
+          GPIO.output(l, LEDOFF)
+      GPIO.output(led[channel], LEDON)
+      display.blit(img[channel], zero)
+      pygame.display.update()
+      timer.start()
 
 def resetScreen():
+  global active
+  active = 0
+  for k,l in led.iteritems():
+    GPIO.output(l, LEDOFF)
   display = pygame.display.get_surface()
   display.blit(img['start'], zero)
   pygame.display.update()
 
-# this line has to be below resetScreen
-timer=threading.Timer(waitTime, resetScreen)
+# Singleton class for global timer
 class Timeout(object):
   timer = None
   _instance = None
 
   def start(self):
     if self.timer != None:
+      print("Restarting timer...\n")
       try:
         self.cancel()
       except:
         pass
+    print(self.timer)
+    print("Starting timer!\n")
+    self.timer = None
     self.timer=threading.Timer(waitTime, resetScreen)
     self.timer.start()
 
   def cancel(self):
+    print("Canceling timer!")
     if self.timer != None:
       try:
         self.timer.cancel()
